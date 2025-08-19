948634e1e02a3038c135cf35b4d1512113e30eac
zhilanc9 <zhilanc129@gmail.com>
2025-08-19 08:08:07 +0000

    1

--- app.py
+++ app.py
@@ -13,5 +13,10 @@ def main():
     #database
     return(render_template("main.html"))
 
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    q = float(request.form.get("q"))
+    return(render_template("dbs.html", r=(-50.6*q)+90.2))
+
 if __name__ == "__main__":
     app.run()
